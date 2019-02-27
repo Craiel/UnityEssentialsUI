e6f049f613403c5590959d14b2fbc1b6d98754bc
Patrick Gustavel <craieldev@gmail.com>
2019-02-27 17:23:35 -0500

    canvas utils, bring to front and drag object

--- Runtime/EssentialCoreUI.cs
+++ Runtime/EssentialCoreUI.cs
@@ -12,5 +12,10 @@ namespace Craiel.UnityEssentialsUI.Runtime
         public const string ComponentMenuFolder = "UI/Craiel";
 
         public const string DefaultAnimatorControllerName = "New Animator Controller";
+        
+        public const int ModalBoxOrder = 99996;
+        public const int SelectFieldBlockerOrder = 99997;
+        public const int SelectFieldOrder = 99998;
+        public const int TooltipOrder = 99999;
     }
 }
